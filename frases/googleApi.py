e8e0eeb03b571d351e7b810bc419b4107087bf6b
Leo Balter <leonardo.balter@gmail.com>
2011-02-07 19:39:45 -0200

    Finally the GoogleAPI is working

--- frases/googleApi.py
+++ frases/googleApi.py
@@ -1,26 +1,37 @@
 # -*- coding: utf-8 -*- 
-import urllib2
+import urllib2, urllib
 from django.utils import simplejson
 
 class GoogleApi:
-    url = "https://www.googleapis.com/language/translate/v2?key="
-    key = "AIzaSyBxHfduvWTZZnNaoQgGLOi1K_ox3gTicOc"
+    url = "https://www.googleapis.com/language/translate/v2"
+    
+    params = {
+        "key": "AIzaSyDdYoOdqec4m4oYzp-oi3WjfOxhT09IGc0"
+    }
     lang1 = "&source=pt&target=en"
     lang2 = "&source=en&target=pt"
     
     def getTranslated(self, query, lang='original' ):
-        if lang == 'original':
-            urlFull = self.url + self.key + self.lang1 + '&q='
-        else:
-            urlFull = self.url + self.key + self.lang2 + '&q='
 
-        urlFinal = urlFull + query;
+        if lang == 'original':
+            self.params.update({
+                "source": "pt",
+                "target": "en",
+                "q"     : query.encode('utf-8'),
+            })
+        else:
+            self.params.update({
+                "source": "en",
+                "target": "pt",
+                "q"     : query.encode('utf-8'),
+            })
+            
+        querystring = urllib.urlencode(self.params)
         
-        request = urllib2.Request(urlFinal, None, {'Referer': 'http://www.iloop.com.br'})
-        try:
-            response = urllib2.urlopen(request)
-            results = simplejson.load(response)
-            if results['data']:
-                return results['data']['translations'][0]['translatedText']
-        except Exception:
-            return ''
+        urlFinal = urllib.urlopen(self.url+"?"+querystring)
+        
+        data = urlFinal.read()
+        
+        results = simplejson.loads(data)
+        if results['data']:
+            return results['data']['translations'][0]['translatedText']
